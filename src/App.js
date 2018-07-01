800688ff7684115e46835e6fed5204477a546ae3
Natalie Ventura Villasana <nat@storj.io>
2018-07-01 19:52:46 -0400

    adds pickCard and unflipCards

--- src/App.js
+++ src/App.js
@@ -36,9 +36,57 @@ class App extends Component {
     }
   }
 
+  pickCard(cardIndex) {
+    if (this.state.deck[cardIndex].isFlipped) {
+      return
+    }
+    let cardToFlip = { ...this.state.deck[cardIndex] };
+    cardToFlip.isFlipped = true;
+
+    let newPickedCards = this.state.pickedCards.concat(cardIndex);
+    let newDeck = this.state.deck.map((card, index) => {
+      if (cardIndex == index) {
+        return cardToFlip;
+      }
+      return card;
+    });
+
+    if (newPickedCards.length == 2) {
+      let card1Index = newPickedCards[0];
+      let card2Index = newPickedCards[1];
+      if (newDeck[card1Index].symbol == newDeck[card2Index].symbol) {
+        setTimeout(
+          this.unflipCards.bind(this, card1Index, card2Index), 1000
+        );
+      }
+      newPickedCards = [];
+    }
+    this.setState({
+      deck: newDeck,
+      pickedCards: newPickedCards,
+    })
+  }
+
+  unflipCards(card1Index, card2Index) {
+    let card1 = { ...this.state.deck[card1Index] }
+    let card2 = { ...this.state.deck[card2Index] }
+    card1.isFlipped = false;
+    card2.isFlipped = false;
+    let newDeck = this.state.deck.map((card, index) => {
+      if (index == card1Index) {
+        return card1;
+      }
+      if (index == card2Index) {
+        return card2;
+      }
+      return card;
+    });
+    this.setState({ deck: newDeck });
+  }
+
   render() {
     let cardsJSX = this.state.deck.map((card, index) => {
-      return <MemoryCard />
+      return <MemoryCard symbol={card.symbol} isFlipped={card.isFlipped} key={index} pickCard={this.pickCard.bind(this, index)} />
     });
 
     return (
