461f84dd4f7a73b243799190ea7ea2014b8879f7
Natalie Ventura Villasana <nat@storj.io>
2018-07-01 18:10:34 -0400

    map state to component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,45 @@ import React, { Component } from 'react';
 import './App.css';
 import MemoryCard from './MemoryCard.js';
 
+function generateDeck() {
+  let symbols = ["∆", "ß", "£", "§", "•", "$", "+", "ø"];
+  let deck = [];
+  for (let i = 0; i < 16; i++) {
+    let card = {
+      isFlipped: false,
+      symbol: symbols[i % 8],
+    }
+    deck.push(card);
+  }
+  shuffle(deck);
+  return deck;
+}
+
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
+}
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      deck: generateDeck(),
+      pickedCards: [],
+    }
+  }
+
   render() {
+    let cardsJSX = this.state.deck.map((card, index) => {
+      return <MemoryCard />
+    });
+
     return (
       <div className="App">
         <header className="App-header">
@@ -11,28 +48,16 @@ class App extends Component {
           <h3 className="subtitle">Match cards to win!</h3>
         </header>
         <div>
-          <MemoryCard />
-          <MemoryCard />
-          <MemoryCard />
-          <MemoryCard />
+          {cardsJSX.slice(0, 4)}
         </div>
         <div>
-          <MemoryCard />
-          <MemoryCard />
-          <MemoryCard />
-          <MemoryCard />
+          {cardsJSX.slice(4, 8)}
         </div>
         <div>
-          <MemoryCard />
-          <MemoryCard />
-          <MemoryCard />
-          <MemoryCard />
+          {cardsJSX.slice(8, 12)}
         </div>
         <div>
-          <MemoryCard />
-          <MemoryCard />
-          <MemoryCard />
-          <MemoryCard />
+          {cardsJSX.slice(12, 16)}
         </div>
       </div>
     );
